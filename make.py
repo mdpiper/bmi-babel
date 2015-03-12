ae5b6b833f2dc6a0fdd2b81069d508347ee49432
mcflugen <mcflugen@gmail.com>
2015-03-12 15:36:03 -0600

    Added --clobber flag to remove existing projects.

--- make.py
+++ make.py
@@ -9,11 +9,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('file', type=argparse.FileType('r'),
                         help='Project description file')
+    parser.add_argument('--clobber', action='store_true',
+                        help='Clobber an existing project')
 
     args = parser.parse_args()
 
     try:
-        make_project(yaml.load(args.file))
+        make_project(yaml.load(args.file), clobber=args.clobber)
     except ProjectExistsError as error:
         print 'The specified project (%s) already exists. Exiting.' % error
 
