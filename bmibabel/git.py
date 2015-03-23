34689bd65b31505d00c2d642f2f306a812308e68
mcflugen <mcflugen@gmail.com>
2015-03-23 14:37:07 -0600

    Added docstrings for functions.

--- bmibabel/git.py
+++ bmibabel/git.py
@@ -1,15 +1,45 @@
 #! /usr/bin/env python
+"""Git-related utility functions."""
+
 import os
 
 from .utils import which, check_output, system, cd, status
 
 
 def git_repo_name(url):
+    """Get the name of a git repository.
+
+    Parameters
+    ----------
+    url : str
+        URL of a git repository.
+
+    Returns
+    -------
+    str
+        Name of the repository.
+    """
     (base, _) = os.path.splitext(os.path.basename(url))
     return base
 
 
 def git_repo_sha(url, git=None, branch='master'):
+    """Get the SHA for a git repository.
+
+    Parameters
+    ----------
+    url : str
+        URL of a git repository.
+    git : str, optional
+        Path to the git program.
+    branch : str, optional
+        Branch of the git repository.
+
+    Returns
+    -------
+    str
+        First 10 characters of the SHA.
+    """
     git = git or which('git')
 
     lines = check_output([git, 'ls-remote', url]).strip().split(os.linesep)
@@ -22,6 +52,19 @@ def git_repo_sha(url, git=None, branch='master'):
 
 
 def git_clone(url, git=None, dir='.', branch='master'):
+    """Clone a git repository.
+
+    Parameters
+    ----------
+    url : str
+        URL of a git repository.
+    git : str, optional
+        Path to the git program.
+    dir : str, optional
+        Path to a folder to clone into.
+    branch : str, optional
+        Branch of the git repository.
+    """
     git = git or which('git')
 
     with cd(dir):
@@ -36,17 +79,48 @@ def git_clone(url, git=None, dir='.', branch='master'):
                 'origin/{branch}'.format(branch=branch)])
 
 
-def git_pull(url, dir='.', branch='master'):
+def git_pull(url, dir='.', branch='master', git=None):
+    """Pull from a git repository.
+
+    Parameters
+    ----------
+    url : str
+        URL of a git repository.
+    git : str, optional
+        Path to the git program.
+    dir : str, optional
+        Path to a folder that contains the git repository.
+    branch : str, optional
+        Branch of the git repository.
+    """
+    git = git or which('git')
+
     with cd(dir):
         system(['git', 'checkout', '-q', branch])
         system(['git', 'pull', 'origin', '-q',
                 'refs/heads/{branch}:refs/remotes/origin/{branch}'.format(branch=branch)])
 
 
-def git_clone_or_update(url, dir='.', branch='master'):
+def git_clone_or_update(url, dir='.', branch='master', git=None):
+    """Clone (or update) a git repository .
+
+    If the repository doesn't not exists locally, clone it from *url*.
+    Otherwise, simply update it.
+
+    Parameters
+    ----------
+    url : str
+        URL of a git repository.
+    git : str, optional
+        Path to the git program.
+    dir : str, optional
+        Path to a folder that contains the git repository.
+    branch : str, optional
+        Branch of the git repository.
+    """
     if os.path.isdir(os.path.join(dir, '.git')):
         status('Updating %s' % url)
-        git_pull(url, dir=dir, branch=branch)
+        git_pull(url, dir=dir, branch=branch, git=git)
     else:
         status('Cloning %s' % url)
-        git_clone(url, dir=dir, branch=branch)
+        git_clone(url, dir=dir, branch=branch, git=git)
