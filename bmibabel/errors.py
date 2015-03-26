e6001697835fd6db8b94ae6c47623ec8ea693621
mcflugen <mcflugen@gmail.com>
2015-03-26 12:27:33 -0600

    Added new exceptions for yaml parsing errors.

--- bmibabel/errors.py
+++ bmibabel/errors.py
@@ -1,3 +1,7 @@
+"""Define errors for the bmibabel package."""
+
+import types
+
 
 class Error(Exception):
 
@@ -26,3 +30,35 @@ class MissingFileError(Error):
 
     def __str__(self):
         return self._fname
+
+
+class ParseError(Error):
+
+    """Raise this exception if unable to parse a file."""
+
+    def __init__(self, msg):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
+
+
+class MissingKeyError(ParseError):
+
+    """Raise this exception if a required key is missing."""
+
+    def __init__(self, key):
+        if not isinstance(key, types.StringTypes):
+            key = ', '.join(key)
+        self._key = key
+
+    def __str__(self):
+        return 'missing keys: %s' % self._key
+
+
+class UnknownKeyError(MissingKeyError):
+
+    """Raise this exception if an unknown key is present."""
+
+    def __str__(self):
+        return 'unknown keys: %s' % self._key
