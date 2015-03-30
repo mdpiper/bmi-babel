b4717aee86d213c455c64225a66ee1305e111aec
mcflugen <mcflugen@gmail.com>
2015-03-30 14:05:53 -0600

    Added missing docstrings.

--- bmibabel/errors.py
+++ bmibabel/errors.py
@@ -15,9 +15,23 @@ class ProjectExistsError(Error):
     """Raise this exception for an already existing project."""
 
     def __init__(self, name):
+        """Initialize with project name.
+
+        Parameters
+        ----------
+        name : str
+            Name of project.
+        """
         self._name = name
 
     def __str__(self):
+        """Convert to string.
+        
+        Returns
+        -------
+        str
+            Name of existing project.
+        """
         return self._name
 
 
@@ -26,9 +40,23 @@ class MissingFileError(Error):
     """Raise this exception if a required file is missing."""
 
     def __init__(self, fname):
+        """Initialize if missing file name.
+
+        Parameters
+        ----------
+        fname : str
+            Name of missing file.
+        """
         self._fname = fname
 
     def __str__(self):
+        """Convert to string.
+        
+        Returns
+        -------
+        str
+            Name of the missing file.
+        """
         return self._fname
 
 
@@ -37,9 +65,23 @@ class ParseError(Error):
     """Raise this exception if unable to parse a file."""
 
     def __init__(self, msg):
+        """Initialize with error message.
+
+        Parameters
+        ----------
+        msg : str
+            Error message.
+        """
         self._msg = msg
 
     def __str__(self):
+        """Convert to string.
+        
+        Returns
+        -------
+        str
+            Error message.
+        """
         return self._msg
 
 
@@ -48,11 +90,25 @@ class MissingKeyError(ParseError):
     """Raise this exception if a required key is missing."""
 
     def __init__(self, key):
+        """Initialize with missing key.
+
+        Parameters
+        ----------
+        key : str or iterable
+            Missing key or keys.
+        """
         if not isinstance(key, types.StringTypes):
             key = ', '.join(key)
         self._key = key
 
     def __str__(self):
+        """Convert to string.
+        
+        Returns
+        -------
+        str
+            Error message with the missing key or keys.
+        """
         return 'missing keys: %s' % self._key
 
 
@@ -61,4 +117,11 @@ class UnknownKeyError(MissingKeyError):
     """Raise this exception if an unknown key is present."""
 
     def __str__(self):
+        """Convert to string.
+        
+        Returns
+        -------
+        str
+            Error message with the unknown key or keys.
+        """
         return 'unknown keys: %s' % self._key
