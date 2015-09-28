cc573c2c5e4e747c312a44e9a97d563ad3fbcf20
mcflugen <huttone@colorado.edu>
2015-09-28 14:15:04 -0600

    Removed reading version from __init__.py.

--- setup.py
+++ setup.py
@@ -1,33 +1,22 @@
-from setuptools import setup, find_packages
-
-from codecs import open
-from os import path
-
-
-_HERE = path.abspath(path.dirname(__file__))
+from setuptools import setup
 
 
 def read(*names, **kwds):
+    from codecs import open
+    from os import path
+
+
+    _HERE = path.abspath(path.dirname(__file__))
+
     return open(
         path.join(_HERE, *names),
         encoding=kwds.get('encoding', 'utf-8')
     ).read()
 
 
-def find_version(*file_paths):
-    import re
-
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
 setup(
     name='bmibabel',
-    version=find_version("bmibabel/__init__.py"),
+    version="0.1.0",
     description='Generate BMI language bindings with babel.',
     long_description=read('README.md'),
     url='https://github.com/csdms/bmi-babel',
